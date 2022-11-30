eec824e8e021e864725f6709348c2a3bcfad9e64
James <jamestariga08@live.com>
2022-11-29 21:52:23 -0800

    🔥 Added the achievement screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './pages/Home'
 import Quiz from './pages/Quiz'
 import ModulesPage from './pages/ModulesPage'
+import Achievement from './pages/Achievement'
 import { RootStackParamList } from './utils/type'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
@@ -14,7 +15,7 @@ export default function App() {
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Quiz' component={Quiz} />
         <Stack.Screen name='ModulesPage' component={ModulesPage} />
-        <Stack.Screen name='Achievements' component={ModulesPage} />
+        <Stack.Screen name='Achievements' component={Achievement} />
       </Stack.Navigator>
     </NavigationContainer>
   )
